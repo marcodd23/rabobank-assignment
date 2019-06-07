a063fb43e68752ad21d6ea415909ddb60293c44b
marco <marco.dd23@gmail.com>
2019-06-07 09:05:16 +0200

    commit 4 removed commented code

--- src/main/java/io/sytac/rabobank/app/utils/FileUtil.java
+++ src/main/java/io/sytac/rabobank/app/utils/FileUtil.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -38,53 +39,4 @@ public class FileUtil {
             e.printStackTrace();
         }
     }
-
-/*    public static String createFileUseJavaNewIO(String filePath)
-    {
-        String fileAbsoultePath = "";
-        try
-        {
-            *//* First create the Path object. *//*
-            Path filePathObj = Paths.get(filePath);
-
-            Path result = Files.createFile(filePathObj);
-
-            System.out.println("Create " + filePath + " success. ");
-
-            *//* Show absolute path to see different filePath case real file location.*//*
-            fileAbsoultePath = result.toFile().getAbsolutePath();
-            System.out.println("File absolute path is " + fileAbsoultePath);
-        }catch(Exception ex)
-        {
-            ex.printStackTrace();
-        }
-        return fileAbsoultePath;
-    }
-    *//* Test createFileUseJavaNewIO(String filePath) method with three scenario.
-     * case1 : file name only.
-     * case2 : absolute path.
-     * case3 : relative path.
-     * case1 and case3 will create the file under project root folder or current java class run folder.
-     * *//*
-    public static void createFileUseJavaNewIOExample()
-    {
-        *//* Create with only file name. *//*
-        String fileName = "dev2qa.com.txt";
-        createFileUseJavaNewIO(fileName);
-
-        *//* Get path separator to make java code platform independent. *//*
-        String fileSep = System.getProperty("file.separator");
-
-        *//* Create with absolute path. *//*
-
-        *//* Get current working directory. *//*
-        String currDir = System.getProperty("user.dir");
-        *//* Build a absolute path. *//*
-        String absoluteFilePath = currDir + fileSep + "test" + fileSep + "dev2qa.com.txt";
-        createFileUseJavaNewIO(absoluteFilePath);
-
-        *//* Create with relative path. *//*
-        String relativeFilePath = "testRelative" + fileSep + "dev2qa.com.txt";
-        createFileUseJavaNewIO(relativeFilePath);
-    }*/
 }
